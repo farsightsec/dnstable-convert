52cb06ef835df27c105cb8ede7afa7baa998e1e7
Chris Mikkelson <cmikk@fsi.io>
2018-04-11 13:31:58 -0500

    Index label-reversed SOA mnames.

--- dnstable_convert.c
+++ dnstable_convert.c
@@ -310,11 +310,19 @@ process_rdata_slice(Nmsg__Sie__DnsDedupe *dns, size_t i, ubuf *key, ubuf *val)
 static void
 process_rdata_name_rev(Nmsg__Sie__DnsDedupe *dns, size_t i, ubuf *key, ubuf *val)
 {
-	size_t offset;
+	size_t offset, len = dns->rdata[i].len;
 	uint8_t name[WDNS_MAXLEN_NAME];
 	wdns_res res;
 
 	switch (dns->rrtype) {
+	case WDNS_TYPE_SOA:
+		res = wdns_len_uname(dns->rdata[i].data,
+				     dns->rdata[i].data + len,
+				     &len);
+		if (res != wdns_res_success)
+			return;
+		printf("SOA record found, len=%zd\n", len);
+		/* fallthrough */
 	case WDNS_TYPE_NS:
 	case WDNS_TYPE_CNAME:
 	case WDNS_TYPE_DNAME:
@@ -323,9 +331,11 @@ process_rdata_name_rev(Nmsg__Sie__DnsDedupe *dns, size_t i, ubuf *key, ubuf *val
 		break;
 	case WDNS_TYPE_MX:
 		offset = 2;
+		len -= offset;
 		break;
 	case WDNS_TYPE_SRV:
 		offset = 6;
+		len -= offset;
 		break;
 	default:
 		return;
@@ -342,9 +352,9 @@ process_rdata_name_rev(Nmsg__Sie__DnsDedupe *dns, size_t i, ubuf *key, ubuf *val
 	ubuf_add(key, ENTRY_TYPE_RDATA_NAME_REV);
 
 	/* key: rdata name (label-reversed) */
-	res = wdns_reverse_name(dns->rdata[i].data + offset, dns->rdata[i].len - offset, name);
+	res = wdns_reverse_name(dns->rdata[i].data + offset, len, name);
 	assert(res == wdns_res_success);
-	ubuf_append(key, name, dns->rdata[i].len - offset);
+	ubuf_append(key, name, len);
 
 	add_entry(dns, key, val);
 }
