fc0be506f0df6a07dbb188710317ce0c881563b0
Demian Vladi <dvladi@domaintools.com>
2024-01-08 08:48:05 -0800

    Code review changes

--- dnstable_convert.c
+++ dnstable_convert.c
@@ -74,10 +74,10 @@ static const struct {
 #endif
 
 static const char		*nmsg_fname;
-static const char		*nmsg_source;
+static const char		*nmsg_source_info;
 static const char		*db_dns_fname;
 static const char		*db_dnssec_fname;
-static bool			store_nmsg_source = false; /* Store nmsg source filename */
+static bool			store_nmsg_source_info = false; /* Store nmsg source info */
 static bool			migrate_dnssec;
 static bool			preserve_empty = false;	/* Keep empty dns files? */
 
@@ -688,15 +688,15 @@ process_version(ubuf *key, ubuf *val)
 }
 
 static void
-process_source(ubuf *key, ubuf *val)
+process_source_info(ubuf *key, ubuf *val)
 {
 	mtbl_res res;
 
 	ubuf_clip(key, 0);
-	ubuf_add(key, ENTRY_TYPE_SOURCE);
-	ubuf_append(key, nmsg_source, strlen(nmsg_source));
+	ubuf_add(key, ENTRY_TYPE_SOURCE_INFO);
+	ubuf_append(key, nmsg_source_info, strlen(nmsg_source_info));
 	ubuf_cterm(key);
-	
+
 	ubuf_clip(val, 0);
 
 	res = mtbl_sorter_add(sorter_dns, ubuf_data(key), ubuf_size(key),
@@ -769,8 +769,8 @@ do_read(void)
 
 	process_time_range(key, val);
 	process_version(key, val);
-	if (store_nmsg_source)
-		process_source(key, val);
+	if (store_nmsg_source_info)
+		process_source_info(key, val);
 
 	ubuf_destroy(&key);
 	ubuf_destroy(&val);
@@ -937,8 +937,8 @@ usage(const char *name)
 	" -c TYPE:  Use TYPE compression (Default: zlib)\n"
 	" -l LEVEL: Use numeric LEVEL of compression.\n"
 	"           Default varies based on TYPE.\n"
-	" -s NAME:  NMSG file name to include in output if input is stdin.\n"
-	" -S:       Include nmsg source in output.\n"
+	" -s NAME:  NMSG source information to include in output if input is stdin.\n"
+	" -S:       Include nmsg source information in output.\n"
 	" -m MMB:   Specify maximum amount of memory to use for in-memory sorting, in megabytes.\n"
 	" -p:       Preserve empty DNS/DNSSEC files.\n");
 }
@@ -990,10 +990,10 @@ main(int argc, char **argv)
 			preserve_empty = true;
 			break;
 		case 'S':
-			store_nmsg_source = true;
+			store_nmsg_source_info = true;
 			break;
 		case 's':
-			nmsg_source = optarg;
+			nmsg_source_info = optarg;
 			break;
 		case 'h':
 		case '?':
@@ -1014,13 +1014,13 @@ main(int argc, char **argv)
 	db_dns_fname = argv[1];
 	db_dnssec_fname = argv[2];
 
-	if (store_nmsg_source && nmsg_source == NULL) {
+	if (store_nmsg_source_info && nmsg_source_info == NULL) {
 		if (strcmp(nmsg_fname, "-") == 0) {
-			fprintf(stderr, "Please provide NMSG source\n");
+			fprintf(stderr, "Please provide NMSG source information\n");
 			usage(name);
 			return (EXIT_FAILURE);
 		}
-		nmsg_source = nmsg_fname;
+		nmsg_source_info = nmsg_fname;
 	}
 
 	show_startup_details(stderr);
